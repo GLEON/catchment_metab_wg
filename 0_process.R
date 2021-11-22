3734b3e411f7f7c985056b4111c7b2348b12c340
Zwart <jzwart@usgs.gov>
2021-11-22 07:46:35 -0500

    updating process functions
    ;

--- 0_process.R
+++ 0_process.R
@@ -1,6 +1,6 @@
 
 
-source('0_process/src/process_functions.R')
+source('0_process/src/process_functions.R', encoding = 'UTF-8')
 
 p0_targets <- list(
 
@@ -16,10 +16,39 @@ p0_targets <- list(
   ),
 
   tar_target(
+    p0_load_dir,
+    'results/nutrient load/'
+  ),
+
+  tar_target(
     p0_metab_timeseries_data,
     collate_metab_timeseries(
-      metab_dir = p0_metab_dir,
-      config = p0_config
+      metab_dir = p0_metab_dir
+    )
+  ),
+
+  tar_target(
+    p0_season_cutoff_data,
+    load_season_cutoff(
+      in_file = 'results/z_scored_schmidt.rds'
+    )
+  ),
+
+  tar_target(
+    p0_load_timeseries_data,
+    collate_load_timeseries(
+      load_dir = p0_load_dir,
+      config = p0_config,
+      metab_data = p0_metab_timeseries_data,
+      metadata_file = 'data/metadataLookUp.csv',
+      season_cutoff_file = 'results/z_scored_schmidt.rds'
+    )
+  ),
+
+  tar_target(
+    p0_inlake_nutrient_timeseries_data,
+    collate_inlake_nutrient_timeseries(
+      inlake_nutrient_file = 'data/in_lake_nutrients/GLEON_nutrient_inlake.csv'
     )
   )
 
